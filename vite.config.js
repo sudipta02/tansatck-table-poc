df7b12be1ede864d6e5803e963eb11fdd335b31b
sudipta02 <sudipta.sundar95@gmail.com>
2024-03-12 17:13:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/tansatck-table-poc/",
   plugins: [react()],
   resolve: {
     alias: {
